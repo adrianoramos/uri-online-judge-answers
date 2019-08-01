b2d7368de19ec4b9a656a817d8db842991a54b70
Adriano Silva <adrianors.web@gmail.com>
2019-08-01 15:11:42 -0300

    Salário

---
+++ Iniciante/1008.py
@@ -0,0 +1,10 @@
+# 1008 - Salário
+
+number = int(input())
+hours = int(input())
+salary_hour = float(input())
+
+salary = hours * salary_hour
+
+print("NUMBER = %d" % number)
+print("SALARY = U$ %.2f" % salary)
