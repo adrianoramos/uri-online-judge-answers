c76d4a875ddb75da23060d037be245969eb2a943
Adriano Silva <adrianors.web@gmail.com>
2019-08-01 15:11:00 -0300

    Média 2

---
+++ Iniciante/1006.py
@@ -0,0 +1,13 @@
+# 1006 - Média 2
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+peso_a = 2
+peso_b = 3
+peso_c = 5
+
+media = (a * peso_a + b * peso_b + c * peso_c) / (peso_a + peso_b + peso_c)
+
+print("MEDIA = %.1f" % media)
