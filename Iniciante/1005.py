f09401c6afcdc0c3cc9f56e569611ecbb31ea0e8
Adriano Silva <adrianors.web@gmail.com>
2019-08-01 15:10:41 -0300

    Média 1

---
+++ Iniciante/1005.py
@@ -0,0 +1,10 @@
+# 1005 - Média 1
+
+valor_a = float(input())
+valor_b = float(input())
+
+peso_a = 3.5
+peso_b = 7.5
+media = (valor_a * peso_a + valor_b * peso_b) / (peso_a + peso_b)
+
+print("MEDIA = %.5f" % media)
